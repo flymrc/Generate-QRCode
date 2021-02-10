81832b7744a0abf282b72a22afa6857289228883
ruichaoma <ruichaoma>
2021-02-10 12:02:32 +0800

    update spotless config

--- openapi-server/src/main/kotlin/com/example/openapi_server/api/UserApi.kt
+++ openapi-server/src/main/kotlin/com/example/openapi_server/api/UserApi.kt
@@ -7,17 +7,34 @@ package com.example.openapi_server.api
 
 import com.example.openapi_server.model.XUser
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
+
 import org.springframework.web.bind.annotation.*
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.beans.factory.annotation.Autowired
+
+import javax.validation.Valid
+import javax.validation.constraints.DecimalMax
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+
+import kotlin.collections.List
+import kotlin.collections.Map
 
 @Validated
 @RequestMapping("\${api.base-path:}")
 interface UserApi {
 
+
     @GetMapping(
-        value = ["/user/current"],
-        produces = ["application/json"]
+            value = ["/user/current"],
+            produces = ["application/json"]
     )
     fun userCurrentGet(): ResponseEntity<XUser> {
         return ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
