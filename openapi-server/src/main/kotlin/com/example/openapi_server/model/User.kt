72f83d2e33d259ab79548d6fc640d027d7900d8a
ruichao.ma <flymrc@gmail.com>
2021-02-18 09:20:57 +0800

    Update schemas (#8)
    Co-authored-by: ruichaoma <ruichaoma>

---
+++ openapi-server/src/main/kotlin/com/example/openapi_server/model/User.kt
@@ -0,0 +1,37 @@
+package com.example.openapi_server.model
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import javax.validation.constraints.DecimalMax
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+import javax.validation.Valid
+
+/**
+ * 
+ * @param objectId 
+ * @param userId 
+ * @param nickname 
+ * @param avatarUrl 
+ */
+data class User(
+
+    @get:NotNull  
+    @field:JsonProperty("objectId") val objectId: kotlin.String,
+
+    @get:NotNull  
+    @field:JsonProperty("userId") val userId: kotlin.String,
+
+    @get:NotNull  
+    @field:JsonProperty("nickname") val nickname: kotlin.String,
+
+    @get:NotNull  
+    @field:JsonProperty("avatarUrl") val avatarUrl: kotlin.String
+) {
+
+}
+
