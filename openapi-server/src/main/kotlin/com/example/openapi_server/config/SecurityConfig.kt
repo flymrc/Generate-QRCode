d24f43e9ff7dc07442b03269a367c229353e3d7f
ruichaoma <ruichaoma>
2021-03-05 17:29:27 +0800

    update

--- openapi-server/src/main/kotlin/com/example/openapi_server/config/SecurityConfig.kt
+++ openapi-server/src/main/kotlin/com/example/openapi_server/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.example.openapi_server.config
 
+import com.example.openapi_server.auth.AuthTokenFilter
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.security.web.access.AccessDeniedHandlerImpl
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -35,6 +37,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter(true) {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
             .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+
+        http.addFilterBefore(AuthTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
     }
 
     fun accessDeniedHandler(): AccessDeniedHandler {
