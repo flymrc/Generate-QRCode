b98412ebe76e50a295777da2b5eee52a269b54f5
ruichaoma <ruichaoma>
2021-03-05 17:30:42 +0800

    update

--- openapi-server/src/main/kotlin/com/example/openapi_server/auth/AuthTokenFilter.kt
+++ openapi-server/src/main/kotlin/com/example/openapi_server/auth/AuthTokenFilter.kt
@@ -1,24 +1,18 @@
 package com.example.openapi_server.auth
 
-
-import org.eclipse.jetty.http.HttpTokens
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-
-
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-
 class AuthTokenFilter : OncePerRequestFilter() {
     override fun doFilterInternal(
-        request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
     ) {
         val auth = Authentication(null)
-        UsernamePasswordAuthenticationToken("", null)
         SecurityContextHolder.getContext().authentication = auth
         filterChain.doFilter(request, response)
     }
